5102249f6cdf15ab2db51d89bfd219eb4dd5ed09
hoytlindsay05 <109611157+hoytlindsay05@users.noreply.github.com>
2024-09-17 19:05:14 -0500

    Update VectorClock.java

--- VectorClock.java
+++ VectorClock.java
@@ -5,5 +5,5 @@ public interface VectorClock extends Remote {
     int[] getClock() throws RemoteException;
     void increment(int processId) throws RemoteException;
     void update(int[] remoteClock) throws RemoteException;
-    int[] merge(int[] clock1, int[] clock2) throws RemoteException; // Optional
+    int[] merge(int[] clock1, int[] clock2) throws RemoteException;
 }
